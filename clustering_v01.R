1c9d2cb925206ce6cb2dabb84612828df65d25e5
shanglai <dalopeznoria@gmail.com>
2017-11-16 19:37:43 -0600

    Update clustering_v01.R

--- clustering_v01.R
+++ clustering_v01.R
@@ -5,6 +5,7 @@ library(magrittr)
 library(e1071)
 library(caret)
 
+set.seed(65414)
 iris
 ggpairs(iris)
 i2 <- iris[,-5]
@@ -18,6 +19,32 @@ i3
 head(i3)
 ggplot(i3) + geom_point(aes(x=PC1,y=PC2,color=especie,alpha=.5))
 
-kmeans.i3 <- kmeans(i3[,-3],3,iter.max = 10)
+#K-means
+n.k <- 3
+kmeans.i3 <- kmeans(i3[,-3],n.k,iter.max = 10)
+ggplot() + geom_point(aes(x=i3$PC1,y=i3$PC2,color=factor(kmeans.i3$cluster),alpha=.5))
+n.k <- 5
+kmeans.i3 <- kmeans(i3[,-3],n.k,iter.max = 10)
+ggplot() + geom_point(aes(x=i3$PC1,y=i3$PC2,color=factor(kmeans.i3$cluster),alpha=.5))
+n.k <- 3
+kmeans.i3 <- kmeans(i3[,-3],n.k,iter.max = 100)
+ggplot() + geom_point(aes(x=i3$PC1,y=i3$PC2,color=factor(kmeans.i3$cluster),alpha=.5))
 
-ggplot() + geom_point(aes(x=i3$PC1,y=i3$PC2,color=kmeans.i3$cluster,alpha=.5))
+#C-means
+n.c <- 3
+cmeans.i3 <- cmeans (i3[-3], n.c, iter.max=10, verbose=FALSE, dist="euclidean")
+ggplot() + geom_point(aes(x=i3$PC1,y=i3$PC2,color=factor(cmeans.i3$cluster),alpha=.5))
+
+n.c <- 3
+cmeans.i3 <- cmeans (i3[-3], n.c, iter.max=100, verbose=FALSE, dist="euclidean",m = 5)
+ggplot() + geom_point(aes(x=i3$PC1,y=i3$PC2,color=factor(cmeans.i3$cluster),alpha=.5))
+
+compara <- data.frame(especie=iris[,5],km=kmeans.i3$cluster,cm=cmeans.i3$cluster)
+head(compara)
+table(compara[,c('especie','km')])
+table(compara[,c('especie','cm')])
+
+
+
+
+
